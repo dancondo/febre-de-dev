961f79aea65263757797b7ec99e407eddbf679de
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-11-24 20:09:20 -0200

    new create edit and update done

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,24 +1,51 @@
 class Admin::PostsController < ApplicationController
   before_action :check_admin
+  before_action :set_post, only: %i[edit update destroy]
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save 
+      redirect_to post_path(@post)
+    else
+      render :new 
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit 
+    end
   end
 
   def destroy
+    if @post.destroy
+      redirect_to posts_path
+    else
+      render :show
+    end
   end
 
   private
 
   def check_admin
-    return user_signed_in? && current_user.admin?
+    return if user_signed_in? && current_user.admin
     redirect_to root_path
+  end
+
+  def set_post 
+    @post = Post.find(params[:id])
+  end 
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
